bc2af373d46813f1bfb8586cc7fea7e288e7109e
huyngse <ngogiahuy775309@gmail.com>
2024-08-16 18:22:14 +0700

    Pins page

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
-const Pins = () => {
+import { useState } from 'react'
+import { CreatePin, Feed, Navbar, PinDetail, Search } from '../components';
+import { Route, Routes } from 'react-router-dom';
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className='h-full'>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
