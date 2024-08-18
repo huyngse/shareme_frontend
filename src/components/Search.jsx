68672e636a6c0ac3eec3f91acc2801dd496ffa86
huyngse <ngogiahuy775309@gmail.com>
2024-08-18 09:12:05 +0700

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import Spinner from './Spinner';
+const Search = ({ searchTerm, setSearchTerm }) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      setLoading(true);
+      if (searchTerm) {
+        const query = searchQuery(searchTerm.toLowerCase());
+        client.fetch(query).then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+      } else {
+        client.fetch(feedQuery).then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+      }
+    }, 500);
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm])
 
-const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+      {loading && <Spinner message={"Searching for pins..."} />}
+      {pins?.length > 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className='mt-10 text-center text-xl'>No Pins Found!</div>
+      )}
+    </div>
   )
 }
 
